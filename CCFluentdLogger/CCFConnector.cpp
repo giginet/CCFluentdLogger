b494ef696b3bfc8b2536a59826476a42005456b9
giginet <giginet.net@gmail.com>
2015-06-07 14:50:02 +0900

    Add CCFConnector(WIP)

---
+++ CCFluentdLogger/CCFConnector.cpp
@@ -0,0 +1,9 @@
+//
+//  CCFConnector.cpp
+//  CCFluentdLogger
+//
+//  Created by giginet on 6/6/15.
+//
+//
+
+#include "CCFConnector.h"
---
+++ CCFluentdLogger/CCFConnector.h
@@ -0,0 +1,22 @@
+//
+//  CCFConnector.h
+//  CCFluentdLogger
+//
+//  Created by giginet on 6/6/15.
+//
+//
+
+#include "CCFluentdMacros.h"
+#include "cocos2d.h"
+
+#ifndef __CCFluentdLogger__CCFConnector__
+#define __CCFluentdLogger__CCFConnector__
+
+NS_LOGGER_BEGIN
+
+class Connector :public cocos2d::Ref {
+};
+
+NS_LOGGER_END
+
+#endif /* defined(__CCFluentdLogger__CCFConnector__) */
